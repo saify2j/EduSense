a7f7abcacdb38ba2608adb35c5132fc29d1e01c5
saikat2990 <bsse0910@iit.du.ac.bd>
2019-04-13 12:19:44 +0600

    edusense video added

---
+++ AspMVCCRUD/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspMVCCRUD.Models;
+
+namespace AspMVCCRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "This is the product page!";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Products in our site!";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AspMVCCRUD/Models/UserContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspMVCCRUD.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+
+        }
+        public DbSet<CourseTable>CourseTables { get; set; }
+        public DbSet<runningCourse>runningCourses { get; set; }
+        public DbSet<RunningCoursesList> RunningCoursesLists { get; set; }
+        public DbSet<StudentsList> StudentsLists { get; set; }
+        public DbSet<TeachersList> TeachersLists { get; set; }
+        public DbSet<TeachersOwnList> TeachersOwnLists { get; set; }
+        public DbSet<TrainerReg> TrainerRegs { get; set; }
+        public DbSet<UserDoneCoursesInfo> UserDoneCoursesInfos { get; set; }
+        public DbSet<UserRunningCoursesInfo> UserRunningCoursesInfos { get; set; }
+        public DbSet<UserLogIn> UserLogIns { get; set; }
+        public DbSet<VideoList> VideoLists { get; set; }
+        public DbSet<StudentPage> StudentPages { get; set; }
+
+    }
+}
